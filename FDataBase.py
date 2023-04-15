9d4affa9d042aa79b3d72addb9813649616e5bf0
boroboro27 <rsborodin@mail.ru>
2023-04-15 20:19:26 +0300

    new project

---
+++ FDataBase.py
@@ -0,0 +1,176 @@
+import sqlite3
+import time
+import math
+from datetime import datetime
+from typing import Tuple
+
+
+class FDataBase:
+    def __init__(self, db: sqlite3.Connection) -> None:
+        self.__db = db
+        self.__cur = db.cursor()
+
+    def getMenu(self) -> list[Tuple]:
+        """
+        Получение списка пунктов меню из БД
+        """        
+        try:
+            self.__cur.execute('SELECT * FROM mainmenu')
+            res = self.__cur.fetchall()
+            if res: return res
+        except sqlite3.Error as err:
+            print(f'Ошибка чтения списка меню из БД - {str(err)}')
+        return []
+    
+    def __getEventType(self, event_type) -> Tuple[int]:
+        """
+        Получение id типа события по его названию
+        """        
+        try:
+            self.__cur.execute('SELECT id FROM event_types WHERE event_type = ?', (event_type,))
+            res = self.__cur.fetchone()
+            if res: return res
+        except sqlite3.Error as err:
+            print(f'Ошибка чтения списка типов событий из БД - {str(err)}')
+        return []
+    
+    def addUser(self, email: str) -> Tuple[bool, int | str]:
+        """
+        Добавление нового пользователя в БД
+        """
+        try:
+            now_dt = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            self.__cur.execute("INSERT INTO users(email) VALUES(?)", (email,))
+            user_id = self.__cur.lastrowid
+            self.__db.commit()
+        except sqlite3.Error as err:
+            print(f'Ошибка добавления пользователя в БД - {str(err)}')
+            return (False, str(err))
+        return (True, user_id)
+    
+    def getUser(self, email: str) -> Tuple[int, int]:
+        """
+        Получение информации о пользователе по email
+
+        :param email: электронный адрес пользователя
+        :return: кортеж с информацией о пользователе (id, принадлежность к администратору(0 или 1))
+        """
+        sql = "SELECT id, is_admin FROM users WHERE email = ?"
+        try:
+            self.__cur.execute(sql, (email.lower(),))
+            res = self.__cur.fetchone()
+            if res: return res
+        except sqlite3.Error as err:
+            print(f'Ошибка чтения пользователя из БД - {str(err)}')
+        return ()    
+    
+    def addBook(self, title: str, author: str, year: int, user_id: int) -> Tuple[bool, int | str]:        
+        """
+        Добавление новой книги в БД
+
+        :params title: название книги, author: автор, year: год издания, status_id: id статуса (1), user_id: id владельца книги
+        :return: кортеж с информацией о добавленной книге (статус добавления(True/False), id книги)
+        """
+        try:            
+            now_dt = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            self.__cur.execute("INSERT INTO books(title, author, public_year, user_id) VALUES(?, ?, ?, ?)", 
+                            (title, author, year, user_id))
+            book_id = self.__cur.lastrowid
+            self.__db.commit()            
+        except sqlite3.Error as err:
+            print(f'Ошибка добавления книги в БД - {str(err)}')
+            return (False, str(err))
+        return (True, book_id)
+    
+    def getBook(self, book_id: int) -> Tuple[str, str, int, str]:
+        """
+        Получает информацию о книге по ее идентификатору
+
+        :param book_id: идентификатор книги
+        :return: кортеж с информацией о книге (название, автор, год издания, email пользователя, который добавил книгу)
+        """
+        # Параметризованный запрос с использованием знака вопроса вместо book_id
+        sql = (f"SELECT t1.title, t1.author, t1.year, t2.email \n" 
+              f"FROM books as t1 JOIN users as t2 ON t1.user_id = t2.id \n"
+              f"WHERE t1.id = ?")
+        try:
+            # Передаем book_id в метод execute() в виде кортежа
+            self.__cur.execute(sql, (book_id,))
+            res = self.__cur.fetchone()
+            if res: return res
+        except sqlite3.Error as err:
+            print(f'Ошибка чтения книги из БД - {str(err)}')
+        return ()
+    
+    def getAllBooks(self) -> list[Tuple[int, str, str, str, str]]:
+        """
+        Получает информацию о полном каталоге книг
+        
+        :return: кортеж с каталогом книг
+        (id книги, название книги, автор книги, жанр, год издания, 
+        дата добавления книги, статус книги)
+        """
+        # запрос
+        sql = (f"SELECT fb.id, fb.msg, u.email, fb.dt, s.status \n" 
+              f"FROM feedbacks as fb \n"
+              f"JOIN users as u ON fb.user_id = u.id \n"
+              f"JOIN fb_statuses as s ON fb.status_id = s.id \n"
+              f"ORDER BY fb.dt DESC")
+        try:            
+            self.__cur.execute(sql)
+            res = self.__cur.fetchall()
+            if res: return res
+        except sqlite3.Error as err:
+            print(f'Ошибка чтения списка меню из БД - {str(err)}')
+        return []
+    
+    def addFeedback(self, msg: str, user_id: int) -> Tuple[bool, int | str]:        
+        """
+        Добавление нового сообщения (обратной связи) в БД
+
+        :params msg: текст сообщения от пользователя, user_id: id пользователя
+        :return: кортеж с информацией о добавленной записи (статус добавления(True/False), id записи)
+        """
+        try:                                
+            self.__cur.execute("INSERT INTO feedbacks(msg, user_id) VALUES(?, ?)", 
+                            (msg, user_id))
+            feedback_id = self.__cur.lastrowid
+            self.__db.commit()            
+        except sqlite3.Error as err:
+            print(f'Ошибка добавления сообщения в БД - {str(err)}')
+            return (False, str(err))
+        return (True, feedback_id)
+    
+    def getAllFeedbacks(self) -> list[Tuple[int, str, str, str, str]]:
+        """
+        Получает информацию о сообщениях пользователей
+        
+        :return: кортеж с информацией о статусе обращений пользователей
+        (id сообщения, текст сообщения, email пользователя, дата обращения, статус обращения)
+        """
+        # запрос
+        sql = (f"SELECT fb.id, fb.msg, u.email, fb.dt, s.status \n" 
+              f"FROM feedbacks as fb \n"
+              f"JOIN users as u ON fb.user_id = u.id \n"
+              f"JOIN fb_statuses as s ON fb.status_id = s.id \n"
+              f"ORDER BY fb.dt DESC")
+        
+        try:            
+            self.__cur.execute(sql)
+            res = self.__cur.fetchall()
+            if res: return res
+        except sqlite3.Error as err:
+            print(f'Ошибка чтения списка меню из БД - {str(err)}')
+        return []
+    
+    def fixEvent(self, event_type: str, object_id: int, user_id: int) -> bool:
+        type_id = self.__getEventType(event_type)[0]        
+        try:            
+            now_dt = datetime.now().strftime("%Y-%m-%d %H:%M:%S")            
+            self.__cur.execute("INSERT INTO events(type_id, object_id, user_id, dt) VALUES(?, ?, ?, ?)", 
+                            (type_id, object_id, user_id, now_dt))            
+            self.__db.commit()            
+        except sqlite3.Error as err:
+            print(f'Ошибка добавления события в БД - {str(err)}')
+            return False
+        return True
---
+++ flask-books.py
@@ -0,0 +1,196 @@
+import os
+import sqlite3
+
+from flask import (Flask, flash, g, redirect, render_template, request,
+                   session, url_for, abort)
+
+from FDataBase import FDataBase, Tuple
+
+
+
+# конфигурация
+SECRET_KEY = '%%%osfljsflef554454545sfefds^fsdf55'
+DEBAG = True
+
+application = Flask(__name__)
+application.config.from_object(__name__)
+
+def connect_db():
+    """
+    Функция для подключения к базе данных.
+
+    Returns:
+    conn: объект подключения к базе данных
+    """
+    conn = sqlite3.connect('ssc-books.db')
+    conn.row_factory = sqlite3.Row
+    return conn
+
+def get_db():
+    """Соединение с БД, если оно еще не установлено
+
+    Returns:
+        g.link_db: _Соединение с БД_
+    """    
+    if not hasattr(g, 'link_db'):
+        g.link_db = connect_db()
+    return g.link_db
+
+#хэндлер на событие - уничтожение контекста запроса
+@application.teardown_appcontext
+def close_db(error):
+    """Закрываем соединение с БД, если оно было установлено
+
+    Args:
+        error: ошибка
+    """    
+    if hasattr(g, 'link_db'):
+        g.link_db.close()
+
+
+@application.route("/", methods=["POST", "GET"])
+def index():
+    if 'userLogged' in session:
+        if request.method == "POST":
+            pass
+        else:
+            db = get_db()
+            dbase = FDataBase(db)
+            return render_template('index.html', title='Каталог книг',menu=dbase.getMenu())
+    else:
+        return redirect(url_for('login'))
+
+
+@application.route("/about")
+def about():
+    if 'userLogged' in session:
+        db = get_db()
+        dbase = FDataBase(db)
+        return render_template('about.html', title='О проекте "Книжный перекресток"', menu=dbase.getMenu())
+    else:
+        return redirect(url_for('login'))
+
+@application.route("/add-book", methods=["POST", "GET"])
+def add_book(): 
+    db = get_db()
+    dbase = FDataBase(db)
+
+    if 'userLogged' in session:
+        if request.method == "POST":            
+                add_userid = dbase.getUser(session['userLogged'])
+                #title, author, year, status, add_userid
+                res = dbase.addBook(request.form["title-book"].strip(), \
+                                    request.form["author-book"].strip(), \
+                                    request.form["year-book"].strip(), add_userid[0])
+                if not res[0]:
+                    flash(f"Ошибка добавления книги в каталог: {res[1]}. Если ошибку не удается устранить, \n"
+                          f"сообщите, пожалуйста, нам об ошибке через форму обратной связи.", category='error')
+                else:
+                    flash((f"Книга успешно добавлена в каталог под номером #{res[1]}. "
+                           f"Запишите или вклейте этот номер в книгу на видное место."), category='success')
+                    
+                    is_fixed = dbase.fixEvent('add_book', res[1], add_userid[0]) 
+                    if not is_fixed:
+                        flash(f"Ошибка при регистрации события. \n"
+                              f"Сообщите, пожалуйста, нам об ошибке через форму обратной связи.", category='error')
+
+        return render_template('add-book.html', title="Зарегистрировать новую книгу", menu=dbase.getMenu())               
+    else:
+        return redirect(url_for('login'))
+
+@application.route('/book/<int:book_id>', methods=["GET"])
+def showBoook(book_id):
+    db = get_db()
+    dbase = FDataBase(db)
+    if 'userLogged' in session: 
+        if dbase.getUser(session['userLogged'])[2] == 0:        
+            title, author, year, user_add = dbase.getBook(book_id)
+            if not title:
+                abort(404)
+            return render_template('book.html', title=title, author=author, year=year, user_add=user_add, menu=dbase.getMenu())
+        else:
+            abort(403)
+    else:
+        return redirect(url_for('login'))
+
+@application.route("/profile/<path:user>")
+def profile(user):
+    return f"{user}, рады вас видеть снова!"
+
+@application.route("/login", methods=["POST", "GET"])
+def login(): 
+    if 'userLogged' in session:
+        return redirect(url_for('index'))
+    else:
+        db = get_db()
+        dbase = FDataBase(db)        
+
+        if request.method == 'POST':
+            email = request.form['email'].lower().strip()
+            is_user = dbase.getUser(email)
+            if not is_user:
+                res = dbase.addUser(email)
+                is_fixed = dbase.fixEvent('new_user', res[1], res[1])
+                                                  
+                if not res[0]:
+                    flash(f"Ошибка при подключении пользователя: {res[1]}. \n"
+                          f"Если не удается устранить ошибку, сообщите, пожалуйста, о ней через форму обратной связи.", \
+                            category='error')
+                elif not is_fixed:
+                    flash(f"Ошибка при регистрации события. \n"
+                          f"Сообщите, пожалуйста, нам об ошибке через форму обратной связи.", category='error') 
+                else:
+                    session['userLogged'] = email 
+                    return redirect(url_for('index'))
+            else:
+                session['userLogged'] = email
+                return redirect(url_for('index'))
+                    
+    return render_template('login.html', title="Авторизация", menu=dbase.getMenu()) 
+
+@application.route("/contact", methods=["POST", "GET"])
+def contact():
+    db = get_db()
+    dbase = FDataBase(db)
+    if 'userLogged' in session:
+        if request.method == "POST":
+            msg = request.form['message'].strip()
+            user_id = dbase.getUser(session['userLogged'])
+            #title, author, year, status, add_userid
+            res = dbase.addFeedback(msg, user_id[0])
+            if not res[0]:
+                flash(f"Ошибка отправки обращения: {res[1]}.", category='error')
+            else:
+                flash((f"Обращение #{res[1]} успешно отправлено. "
+                       f"Ожидайте ответа на адрес вашей эл. почты {session['userLogged']}"), category='success')
+                
+                is_fixed = dbase.fixEvent('add_feedback', res[1], user_id[0])
+                if not is_fixed:
+                        flash(f"Ошибка при регистрации события. \n"
+                              f"Сообщите, пожалуйста, нам об ошибке через форму обратной связи.", category='error')        
+        
+        return render_template('contact.html', title="Обратная связь", menu=dbase.getMenu())
+    else:
+        return redirect(url_for('login'))
+    
+@application.route("/exit", methods=["GET"])
+def exit():
+    session.clear()
+    return redirect(url_for('login'))
+    
+@application.errorhandler(404)
+def page_not_found(error):
+    db = get_db()
+    dbase = FDataBase(db)
+    return render_template('page404.html', title='Страница не найдена', menu=dbase.getMenu()), 404
+
+@application.errorhandler(403)
+def forbidden(error):
+    db = get_db()
+    dbase = FDataBase(db)
+    return render_template('page403.html', title='Доступ к информации ограничен, т.к. вы не являетесь администратором ресурса.', \
+                           menu=dbase.getMenu()), 403
+
+if __name__ == "__main__":
+   application.run(host='0.0.0.0', debug=DEBAG)
+    
